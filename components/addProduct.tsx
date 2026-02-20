937b4c6bd560aabf41e6f3c77e936f4dc005b965
Petra Paulin <petra.oster.paulin@gmail.com>
2026-02-20 12:19:46 +0100

    Revert "Merge pull request #20 from seanie1995/bugfix/lucide-v"
    This reverts commit ffbf3a3690fc0ea1f30fa9e6ded6f7d885d325eb, reversing
    changes made to b05042bc0f60f0dde0f79f80b45a7ea72bc1a29b.

--- components/addProduct.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const addProduct = () => {
-  return <div>addProduct</div>;
-};
-
-export default addProduct;
--- components/productStats.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const productStats = () => {
-  return <div>productStats</div>;
-};
-
-export default productStats;

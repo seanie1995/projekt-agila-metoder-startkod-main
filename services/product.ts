418e966d55d9bae455ea77ac67c4f0cf09b3ffc9
4pooled <edchallis@protonmail.com>
2026-02-22 20:08:11 +0100

    added create, update and delete functions

--- services/product.ts
+++ services/product.ts
@@ -2,17 +2,51 @@ import type { ProductsResponse } from "./types";
 
 const API_URL = "http://localhost:4000";
 
-export const getProducts = async (): Promise<ProductsResponse> => {
+export const getProducts = async (
+  limit=6,
+  page=1,
+  sort="id",
+  order="asc",
+  query=""
+): Promise<ProductsResponse> => {
   const params = new URLSearchParams({
-    _limit: 6,
-    _page: 1,
-    _sort: "id",
-    _order: "asc"
+    _limit: limit,
+    _page: page,
+    _sort: sort, // sort by id, title, price, etc
+    _order: order, // either "asc" or "desc"
+    _expand: "category",
+    title_like: query // can only search for titles for now
   });
 
   const data = await fetch(
     `${API_URL}/products/?${params}`
-  ).then((res) => res.json());
+  ).then(res => res.json());
 
   return data;
+}
+
+// required keys:
+// title, price, description, thumbnail, categoryId, brand
+export const createProduct = async (product: Product): void => {
+  const res = await fetch(`${API_URL}/products`, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(product)
+  })
+
+  console.log(await res.json());
+}
+
+// updates can for example look like:
+// { title: "New Title", price: 69 }
+export const updateProductByID = async (id: number, updates): void => {
+  await fetch(`${API_URL}/products/${id}`, {
+    method: "PATCH",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(updates)
+  });
+}
+
+export const deleteProductByID = async (id: number): void => {
+  const res = await fetch(`${API_URL}/products/${id}`, { method: "DELETE" });
 }
